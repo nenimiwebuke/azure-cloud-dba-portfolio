7a8d64d33fb837749bd9598b9f7db62b740d3ebd
Nenim Iwebuke <nenimiwebuke@gmail.com>
2026-06-07 19:32:12 -0400

    Added Azure VNet and Subnet using Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~>4.0"
     }
   }
 }
@@ -15,4 +15,18 @@ provider "azurerm" {
 resource "azurerm_resource_group" "portfolio" {
   name     = "rg-cloud-dba-portfolio-dev"
   location = "East US"
+}
+
+resource "azurerm_virtual_network" "portfolio_vnet" {
+  name                = "vnet-cloud-dba-dev"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.portfolio.location
+  resource_group_name = azurerm_resource_group.portfolio.name
+}
+
+resource "azurerm_subnet" "portfolio_subnet" {
+  name                 = "subnet-cloud-dba-dev"
+  resource_group_name  = azurerm_resource_group.portfolio.name
+  virtual_network_name = azurerm_virtual_network.portfolio_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
