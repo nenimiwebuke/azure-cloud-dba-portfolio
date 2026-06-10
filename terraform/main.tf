142a6e652774ff83b288bac25337285f66679ad5
Nenim Iwebuke <nenimiwebuke@gmail.com>
2026-06-10 19:57:35 -0400

    Add NSG and associate with subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
- backend "azurerm" {
-  resource_group_name  = "rg-tfstate-cloud-dba"
-  storage_account_name = "stcloudbdatfstate6869"
-  container_name       = "tfstate"
-  key                  = "cloud-dba-portfolio.tfstate"
-}	
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate-cloud-dba"
+    storage_account_name = "stcloudbdatfstate6869"
+    container_name       = "tfstate"
+    key                  = "cloud-dba-portfolio.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -35,4 +35,27 @@ resource "azurerm_subnet" "portfolio_subnet" {
   resource_group_name  = azurerm_resource_group.portfolio.name
   virtual_network_name = azurerm_virtual_network.portfolio_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "portfolio_nsg" {
+  name                = "nsg-cloud-dba-dev"
+  location            = azurerm_resource_group.portfolio.location
+  resource_group_name = azurerm_resource_group.portfolio.name
+
+  security_rule {
+    name                       = "Deny-All-Inbound"
+    priority                   = 4096
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "portfolio_subnet_nsg" {
+  subnet_id                 = azurerm_subnet.portfolio_subnet.id
+  network_security_group_id = azurerm_network_security_group.portfolio_nsg.id
 }
